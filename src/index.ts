c7629788209a10eedc343110fe1a399ee6d9c81e
Enzo Gutiérrez Pereira <enzogp2005@gmail.com>
2026-07-08 22:16:38 -0300

    stats route

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,22 @@ import { Hono } from "hono";
 const app = new Hono<{ Bindings: CloudflareBindings }>();
 
 app.get("/message", (c) => {
-  return c.text("Hello Hono!");
+    return c.text("Hello Hono!");
 });
+
+app.get("/api/v1/stats/:username", async (c) => {
+    try {
+        const username = c.req.param('username')
+        const res = await fetch(`https://api.github.com/users/${username}`, {
+            headers: {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.5 Safari/537.36"
+            }
+        })
+        const data = await res.json()
+        return c.json({data})
+    } catch (error: any) {
+        return c.json({error: error.message})
+    }
+})
 
 export default app;
