b76be631a50eacd0188b3f38b5d017b1a34bdcaf
Enzo Gutiérrez Pereira <enzogp2005@gmail.com>
2026-07-09 22:56:01 -0300

    cache on banner

--- src/components/router.tsx
+++ src/components/router.tsx
@@ -4,6 +4,13 @@ import UserBanner from "./UserBanner";
 const componentRouter = new Hono<{ Bindings: CloudflareBindings }>();
 
 componentRouter.get("/banner", async (c) => {
+    const cache = caches.default
+    const cacheKey = new Request(c.req.url, c.req.raw)
+
+    const cached = await cache.match(cacheKey)
+    if (cached) {
+        return cached
+    }
     const user = c.req.query("username")
     const bgColor = c.req.query("color")
     if(!user) {
@@ -11,10 +18,16 @@ componentRouter.get("/banner", async (c) => {
     }
     const svg = await UserBanner({username: user, token: c.env.GITHUB_TOKEN, bgColor: (bgColor as string)})
 
-    return c.body(svg, 200, {
-        "Content-Type": "image/svg+xml",
-        "Cache-Control": "public, max-age=3600"
+    const response = new Response(svg, {
+        status: 200,
+        headers: {
+            "Content-Type": "image/svg+xml",
+            "Cache-Control": "public, max-age=21600"
+        }
     })
+    
+    c.executionCtx.waitUntil(cache.put(cacheKey, response.clone()))
+    return response
 })
 
 export default componentRouter;
